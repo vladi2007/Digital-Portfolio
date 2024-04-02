c8a72d8d43f294445e191ccb1db9df004db4baeb
Yura <112415974+men4ikyura@users.noreply.github.com>
2024-04-02 18:59:46 +0500

    improve

---
+++ registration 02.04.24/registatrion-form.js
@@ -0,0 +1,36 @@
+// checks the validity of the form
+
+const password = document.getElementById('password-field');
+const checkPassword = document.querySelector('.show-password');
+const passwordMessage = document.querySelector('.passwordMessage');
+const form = document.getElementById('registration-form');
+
+//check validity form
+form.addEventListener('input', checkValidity)
+
+//function for check validity all fields
+function checkValidity(event) {
+    const formNode = event.target.form
+    const isValid = formNode.checkValidity()
+    formNode.querySelector('button').disabled = (!isValid || password.value.length < 8 || password.value.length > 20);
+  }
+
+
+//check length password
+password.oninput = function (){
+    if (password.value.length < 8)
+      passwordMessage.textContent = 'Пароль должен содержать не менее 8 символов.';
+    else if (password.value.length > 20)
+      passwordMessage.textContent = 'Пароль должен содержать не более 20 символов.';
+    else
+      passwordMessage.textContent = '';
+  }
+  
+//show password
+checkPassword.onchange = function(){
+if (checkPassword.checked)
+    password.type = "text";
+else
+    password.type = "password";
+}
+  
