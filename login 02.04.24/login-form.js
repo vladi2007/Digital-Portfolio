e5f19b0c3a80402306901afaebe8b3a3a7558800
Yura <112415974+men4ikyura@users.noreply.github.com>
2024-04-02 19:01:12 +0500

    improve

---
+++ login 02.04.24/login-form.js
@@ -0,0 +1,19 @@
+// check validity of form
+
+const form = document.getElementById('entrance-form');
+const checkPassword = document.querySelector('.show-password');
+const password = document.getElementById('password-field');
+
+//show password
+checkPassword.onchange = function(){
+    if (checkPassword.checked)
+        password.type = "text";
+    else
+        password.type = "password";
+}
+//function of check validity
+function checkValidity(event) {
+    const formNode = event.target.form
+    const isValid = formNode.checkValidity()
+    formNode.querySelector('button').disabled = !isValid;
+  }
