dec5071aa9110e8594d09a55b30f483d63a821e7
Yura <112415974+men4ikyura@users.noreply.github.com>
2024-04-29 19:35:17 +0500

    Add files via upload

--- static/js/profileControler.js
+++ static/js/profileControler.js
@@ -1,31 +1,4 @@
 
-// fetch("/auth/is_authenticated")
-//     .then(response => {
-//         if (response != 200)
-//         {
-//             let link2 = document.getElementById("link-2");
-//             let link3 = document.getElementById("link-3")
-//             let link4 = document.getElementById("link-4")
-//             link2.style.display = 'none';
-//             link3.innerText = "Вход"
-//             link3.href = "/login"
-//             link3.style.marginLeft = "calc(50% - 130px)";
-//             link4.innerText = "Регистрация"
-//             link4.href = "/register"
-//             changeStyle()
-//         }
-//         else{
-//             checkOwnerProfile()
-//             .then(isMyprofile => {
-//                 if (!isMyprofile) {
-//                     changeStyle();
-//                     let link2 = document.getElementById("link-2");
-//                     link2.href = "profile/id"
-//                 }
-//             });
-//         }
-//     });
-
 checkOwnerProfile()
 
 function checkOwnerProfile(){
--- static/js/profileSendProfInfo.js
+++ static/js/profileSendProfInfo.js
@@ -10,10 +10,29 @@
 // если на своей странице, то разрешаем редактирование
 // если не на своей странице, то скрываем кнопки
 
+let formDataProfile1 = document.getElementById("form-1")
 let formDataProfile2 = document.getElementById("form-2")
-let btnSend = document.getElementById("send-other-data-profile")
 
 
+formDataProfile1.addEventListener('submit', async event =>{
+  event.preventDefault(); 
+  const formData = new FormData(formDataProfile1); 
+  const formDataObject = {
+    first_name: formData.get('name'), 
+    birth_date: formData.get('date'),
+    last_name: formData.get('surname'),
+    sex: formData.get('sex'),
+    city: formData.get('city')
+  }
+  const response = await sendData(formDataObject, '/change_basic_information');
+  if (response.status == 200)
+  {
+    alert("Данные изменены")
+    location.reload();
+  }
+  else 
+    alert("Не получилось")
+})
 
 // send data after click on button
 formDataProfile2.addEventListener('submit', async event => {
@@ -24,19 +43,23 @@ formDataProfile2.addEventListener('submit', async event => {
     tg: formData.get('tg'),
     vk: formData.get('vk'),
     education: formData.get('education'),
-    work: formData.get('work')
+    work: formData.get('work'),
+    email: formData.get('email')
   };
-  const response = await sendData(formDataObject);
+  const response = await sendData(formDataObject, '/send_additional_information');
   if (response.status == 200)
+  {
     alert("Данные изменены")
+    location.reload();
+  }
   else 
     alert("Не получилось")
   
 
 });
 //generate post-request
-async function sendData(data) {
-  return fetch('/send_additional_information', {
+async function sendData(data, ull) {
+  return fetch(ull, {
     method: 'PUT',
     headers:
     { 'Content-Type': 'application/json'},
