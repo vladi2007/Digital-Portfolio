25b25e3fef1ac23f1eec80056e39bb0a0b004e88
Artyom Tretyakov <83952425+mrBoB1k@users.noreply.github.com>
2024-05-16 19:25:26 +0500

    Add files via upload

--- static/js/search.js
+++ static/js/search.js
@@ -1,5 +1,17 @@
-document.addEventListener('keydown', event => {
+fetch("/search/users", {
+    method: 'POST',
+})
+.then(response => response.json())
+.then(data => {
+    if (data.status != 404)
+        ViewProFiles(data)
+    else{
+        DeleteProfiles();
+        DoNotFound();
+    }
+});
 
+document.addEventListener('keydown', event => {
     if (event.key === 'Enter' && event.target.tagName !== 'TEXTAREA') {
         event.preventDefault();
         const form = document.getElementById("form-2");
@@ -40,66 +52,65 @@ document.addEventListener('keydown', event => {
                 DoNotFound();
             }
         });
-
-
-        function ViewProFiles(data){
-            DeleteProfiles();
-            for (let i = 0; i < data.length; i++) {
-                DoProfiles(data[i])
-        }  
-        }
-
-        function DeleteProfiles(){
-            var items = document.querySelectorAll('.user-link');
-            items.forEach(function(item) {
-            item.remove()});
-        }
-
-        function DoProfiles(data){
-            const newListElement = document.createElement('li');
-            newListElement.className = 'el';
-
-            const newLink = document.createElement('a');
-            newLink.className = 'user-link';
-            newLink.href = `/profile/${data.id}`;
-
-            const newImage = document.createElement('img');
-            newImage.className = 'avatar-search';
-            newImage.id = 'ava-new';
-            newImage.src = `/avatar/${data.id}`;
-
-            const newSection = document.createElement('div');
-            newSection.className = 'section';
-
-            const newParagraph = document.createElement('p');
-            newParagraph.className = 'big-info';
-
-            const newSpans = [
-                document.createElement('span'),
-                document.createElement('span'),
-                document.createElement('span'),
-                // document.createElement('span')
-            ];
-
-            newSpans[0].textContent = data.first_name;
-            newSpans[1].textContent = data.last_name;
-            newSpans[2].textContent = data.city;
-            
-            newParagraph.append(...newSpans);
-            newSection.appendChild(newParagraph);
-            newLink.appendChild(newImage);
-            newLink.appendChild(newSection);
-            newListElement.appendChild(newLink);
-            const list = document.querySelector('.list');
-            list.appendChild(newListElement);
-        }
-
-        function DoNotFound(){
-            const newListElement = document.createElement('li');
-            newListElement.className = 'el';
-            const newParagraph = document.createElement('p');
-            newParagraph.className = 'big-info';
-            newParagraph.textContent = "По вашему запросу никто не найден"
-        }
     }
 });
+
+function ViewProFiles(data){
+    DeleteProfiles();
+    for (let i = 0; i < data.length; i++) {
+        DoProfiles(data[i])
+}  
+}
+
+function DeleteProfiles(){
+    var items = document.querySelectorAll('.el');
+    items.forEach(function(item) {
+    item.remove()});
+}
+
+function DoProfiles(data){
+    const newListElement = document.createElement('li');
+    newListElement.className = 'el';
+
+    const newLink = document.createElement('a');
+    newLink.className = 'user-link';
+    newLink.href = `/profile/${data.id}`;
+
+    const newImage = document.createElement('img');
+    newImage.className = 'avatar-search';
+    newImage.id = 'ava-new';
+    newImage.src = `/avatar/${data.id}`;
+
+    const newSection = document.createElement('div');
+    newSection.className = 'section';
+
+    const newParagraph = document.createElement('p');
+    newParagraph.className = 'big-info';
+
+    const newSpans = [
+        document.createElement('span'),
+        document.createElement('span'),
+        document.createElement('span'),
+        // document.createElement('span')
+    ];
+
+    newSpans[0].textContent = data.first_name;
+    newSpans[1].textContent = data.last_name;
+    newSpans[2].textContent = data.city;
+    
+    newParagraph.append(...newSpans);
+    newSection.appendChild(newParagraph);
+    newLink.appendChild(newImage);
+    newLink.appendChild(newSection);
+    newListElement.appendChild(newLink);
+    const list = document.querySelector('.list');
+    list.appendChild(newListElement);
+}
+
+function DoNotFound(){
+    const newListElement = document.createElement('li');
+    newListElement.className = 'el';
+    const newParagraph = document.createElement('p');
+    newParagraph.className = 'big-info';
+    newParagraph.textContent = "По вашему запросу никто не найден"
+}
