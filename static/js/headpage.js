c6b96424b590efc25b89b39d2c30e74778081aae
Yura <112415974+men4ikyura@users.noreply.github.com>
2024-05-16 20:57:55 +0500

    Add files via upload

--- static/js/headpage.js
+++ static/js/headpage.js
@@ -10,11 +10,12 @@ function changeStyle(){
     let link3 = document.getElementById("link-3")
     let link4 = document.getElementById("link-4")
     let link5 = document.getElementById("link-5")
+    let link6 = document.getElementById("link-6")
     link2.style.display = 'none';
-    link3.innerText = "Вход"
-    link3.href = "/login"
-    link3.style.marginLeft = "calc(50% - 130px)";
+    // link3.innerText = "Вход"
+    // link3.href = "/login"
+    // link3.style.marginLeft = "calc(50% - 130px)";
     link4.classList.toggle("hidden-button")
     link5.classList.toggle("hidden-button")
+    link6.classList.toggle('hidden-button')
 }
-
--- static/js/search.js
+++ static/js/search.js
@@ -113,4 +113,29 @@ function DoNotFound(){
     const newParagraph = document.createElement('p');
     newParagraph.className = 'big-info';
     newParagraph.textContent = "По вашему запросу никто не найден"
+    const list = document.querySelector('.list');
+    list.appendChild(newListElement);
+}
+
+
+fetch("/auth/is_authenticated")
+    .then(response => {
+        if (response.status != 200)
+            changeStyle()
+    });
+
+
+function changeStyle(){
+    let link2 = document.getElementById("link-2")
+    let link3 = document.getElementById("link-3")
+    let link4 = document.getElementById("link-4")
+    let link5 = document.getElementById("link-5")
+    let link6 = document.getElementById("link-6")
+    link2.style.display = 'none';
+    // link3.innerText = "Вход"
+    // link3.href = "/login"
+    // link3.style.marginLeft = "calc(50% - 130px)";
+    link4.classList.toggle("hidden-button")
+    link5.classList.toggle("hidden-button")
+    link6.classList.toggle('hidden-button')
 }
